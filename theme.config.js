b1b9412ec61a2aba4107fb39cc3e9fad8fe8eb52
wcgordon1 <wcgordon1@gmail.com>
2022-11-11 18:47:45 -0800

    minor changes, time to eat dinner

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Will Gordon 2023 *11-11-22.
+      <time>{YEAR}</time> © Will Gordon.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
