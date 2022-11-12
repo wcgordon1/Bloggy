e52d38a61515175c7dde9efbf8dcdfd596670572
wcgordon1 <wcgordon1@gmail.com>
2022-11-11 21:30:15 -0800

    work?

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Will Gordon.
+      <time>{YEAR}</time> © Will X.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
